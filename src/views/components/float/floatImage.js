4c758a3aebebb3aa44b8a52ec2d600043d80e91e
Fabio Haddad <fabiohpaes@gmail.com>
2021-07-09 20:22:54 -0300

    [LP-StarWars] Implementation structure

---
+++ src/views/components/float/floatImage.js
@@ -0,0 +1,9 @@
+import yodaBaby from '../../../assets/images/elements/baby-yoda.png';
+
+let floatImage = `
+<div>
+    <img src="${yodaBaby}" alt="Yoda Baby" width="120px" heigth="auto" />
+</div>
+`
+
+export default floatImage;
---
+++ src/views/components/sections/homeSectionOne.js
@@ -0,0 +1,14 @@
+import Logo from '../../../assets/images/logo/logo.png';
+import backgroundOne from '../../../assets/images/background/background1.png';
+
+
+let HomeSectionOne = `
+<section class="section background--one" style="background-image:url(${backgroundOne})">
+    <div class="block-section--one">
+        <h1>CLIQUE NO BABY YODA, ELE TE MOSTRARÁ O CAMINHO</h1>
+        <img class="logo" src=${Logo} alt="Logo Star Wars" width="300px" heigth="auto" />
+    </div>
+</section>
+`
+
+export default HomeSectionOne
