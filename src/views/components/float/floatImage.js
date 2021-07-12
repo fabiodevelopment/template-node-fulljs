d729a5a705354a3bc68443548a9e43627556ba6e
Fabio Haddad <fabiohpaes@gmail.com>
2021-07-12 18:29:24 -0300

    [LP-StarWars] Implementation SCSS in Section One and Two

--- src/views/components/float/floatImage.js
+++ src/views/components/float/floatImage.js
@@ -1,7 +1,7 @@
 import yodaBaby from '../../../assets/images/elements/baby-yoda.png';
 
 let floatImage = `
-<div>
+<div class="baby-yoda" style="position:fixed; left:10px; bottom:10px">
     <img src="${yodaBaby}" alt="Yoda Baby" width="120px" heigth="auto" />
 </div>
 `
--- src/views/components/footer/index.js
+++ src/views/components/footer/index.js
@@ -1,12 +1,12 @@
 import Planet from '../../../assets/images/elements/planet.png'
 
 let footer = `
-    <div>
+    <div class="footer">
         <p>
             Star wars  -  este é um exemplo criado para os Alunos Gama Academy
             Módulo de boas práticas.
         </p>
-        <img src="${Planet}" alt="Planet" width="auto" heigth="180px" />
+        <img src="${Planet}" alt="Planet" width="130px" heigth="auto" />
     </div>
 `
 export default footer
